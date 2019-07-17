e4b35c7f452bad67f895fff927cceb274b74cb57
Kobi Gurkan <kobigurk@gmail.com>
2019-07-17 14:56:51 +0300

    updates genesis extra with relevant lengths

--- packages/celotool/src/lib/generate_utils.ts
+++ packages/celotool/src/lib/generate_utils.ts
@@ -145,7 +145,8 @@ export const generateGenesisFromEnv = (enablePetersburg: boolean = true) => {
 
 const generateIstanbulExtraData = (validators: Validator[]) => {
   const istanbulVanity = 32
-  const signatureVanity = 65
+  //const signatureVanity = 65
+  const blsSignatureVanity = 192
 
   return (
     '0x' +
@@ -157,8 +158,9 @@ const generateIstanbulExtraData = (validators: Validator[]) => {
         validators.map((validator) => Buffer.from(validator.BLSPublicKey, 'hex')),
         [],
         [],
-        Buffer.from(repeat('0', signatureVanity * 2), 'hex'),
-        [],
+        Buffer.from(repeat('0', blsSignatureVanity * 2), 'hex'),
+        Buffer.from(repeat('0', validators.length * 2), 'hex'),
+        Buffer.from(repeat('0', blsSignatureVanity * 2), 'hex'),
       ])
       .toString('hex')
   )
